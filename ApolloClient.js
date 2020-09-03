4936f16cf56461d6c50738d02811b407896af524
Kirill <RA8eYvkfG8w2xGE>
2020-09-03 16:52:06 +0500

    Apollo client customize

--- ApolloClient.js
+++ ApolloClient.js
@@ -3,9 +3,12 @@ import clientConfig from './clientConfig'
 
 const client = new ApolloClient({
   link: createHttpLink({
-    url: clientConfig.graphqlUrl
+    uri: clientConfig.graphqlUrl,
+
   }),
+  //url: clientConfig.graphqlUrl,
   cache: new InMemoryCache()
+
 })
 
 export default client
--- pages/wcShop/index.js
+++ pages/wcShop/index.js
@@ -1,6 +1,40 @@
 import { ShopLayout } from '../../Layout/ShopLayout/ShopLayout'
 import Product from '../../components/wcShopComponent/product'
 import clientConfig from '../../clientConfig'
+import client from '../../ApolloClient'
+import gql from 'graphql-tag'
+
+const PRODUCTS_QUERY =gql`query{
+ products {
+    nodes {
+      ... on SimpleProduct {
+        id
+        productId
+        name
+        slug
+        image {
+          title
+          uri
+          srcSet
+          sourceUrl
+        }
+        onSale
+        price
+        regularPrice
+        salePrice
+        description
+        galleryImages {
+          nodes {
+            sourceUrl
+            srcSet
+            title
+            uri
+          }
+        }
+      }
+    }
+  }
+}`
 
 const WcShop = ({products: serverSideProducts}) => {
   console.log(serverSideProducts)
@@ -8,14 +42,14 @@ const WcShop = ({products: serverSideProducts}) => {
       <ShopLayout title={'Main'}>
         <div className={'flexWrapper'}>
           <div className="container">
-            <div className="goodsContainer">
+           {/* <div className="goodsContainer">
               {serverSideProducts.length
                   ? serverSideProducts.map(((product,index) => {
                     return <Product key={index} props={product}/>
                   }))
                   : ''
               }
-            </div>
+            </div>*/}
           </div>
 
         </div>
@@ -24,10 +58,18 @@ const WcShop = ({products: serverSideProducts}) => {
 }
 
 WcShop.getInitialProps = async () =>{
-  const res = await fetch(`${clientConfig.siteUrl}/wcShop/getProducts`)
+  /*const res = await fetch(`${clientConfig.siteUrl}/wcShop/getProducts`)
   const productData = await res.json()
   return {
     products: productData
+  }*/
+
+  const result = await client.query({
+    query: PRODUCTS_QUERY
+  })
+
+  return {
+    products: result.data.products.nodes
   }
 
 }
