ae2003036ef0f18e73a6c7d9c93818ab4b1f59b9
mpsk8501 <mpak8501@gmail.com>
2020-09-05 09:40:30 +0500

    add config example

---
+++ ConfigDir__example/wooConfig.js
@@ -0,0 +1,7 @@
+const wooConfig = {
+  consumerKey: 'XXX',
+  consumerSecret: 'XXXX',
+  siteUrl: 'http://(word press url)'// e.g. 'http://wordpressSiteUrl.com/graphql' - Your WordPress site url followed by '/graphql'
+}
+
+module.exports = wooConfig
